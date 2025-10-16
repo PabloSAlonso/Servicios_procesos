9779dd960c0561dde524144a47a338865d544021
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-10-16 08:58:57 +0200

    changes

---
+++ BoletinT4_Servidores/Ejercicio2/Ejercicio2.cs
@@ -0,0 +1,31 @@
+﻿using BoletinT4_Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Ejercicio7_8;
+
+namespace BoletinT4_Servicios
+{
+    public class Ejercicio2
+    {
+        //List<Astro> lista = Ejercicio7_8.Principal.colAstros;
+    
+    }
+}
+
+//Ejercicio 2
+//Ampliar el programa gestor de Astros (Ejercicio 8 de los temas 1, 2
+//y 3 genéricos) de forma que guarde la información de la base de
+//datos que se está creando.
+//La colección de Astros debe cargarse al iniciar el programa y
+//guardarse en el disco duro al finalizar el mismo de forma
+//transparente al usuario (el usuario no participa). Esos son los únicos
+//cambios en el programa, al inicio y fin del mismo, no cambies nada
+//más. Hazlo en la carpeta %APPDATA% del usuario.
+//Si no hiciste el ejercicio completo hazlo al menos con la posibilidad
+//de disponer de una colección donde se puedan añadir y eliminar
+//Planetas y Cometas para poder hacer el apartado de archivos.
+
+//En este ejercicio igual tienes q hacer una nueva pero cada vez q inicias q se guarde como en uno de los de Iago (nose ahora le pregunto)
---
+++ BoletinT4_Servidores/Ejercicio2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
