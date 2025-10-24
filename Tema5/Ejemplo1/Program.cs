83ff797fb1fbf5fff07401fe4491c74f4dc90ea8
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-10-24 13:26:10 +0200

    changes

---
+++ Tema5/Ejemplo1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejemplo1
+{
+    internal class Program
+    {
+        static void charA()
+        {
+            for (int i = 1; i < 1000; i++)
+            {
+                Console.Write('A');
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(charA);
+            thread.Priority = ThreadPriority.Highest;
+            thread.Start();
+            //char B
+            for (int i = 1; i < 1000; i++)
+            {
+                Console.Write('B');
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
