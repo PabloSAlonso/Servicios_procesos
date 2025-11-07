51e1c1cd1bc94b7ff283c3a823c6256f14632fd1
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-11-07 10:23:48 +0100

    primeros intentos hilos

--- BoletinHilos/Ejercicio1/Program.cs
+++ BoletinHilos/Ejercicio1/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace Ejercicio1
+﻿using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+using System.Threading;
+
+namespace Ejercicio1
 {
-    internal class Program
+    public class Program
     {
+        //Ejercicio 1
+        //a) Crea dos hilos(Thread) compitiendo que traten de incrementar(el primer hilo) o
+        //decrementar(el segundo hilo) en 1 unidad una variable que comienza en 0.
+        //Funcionarán de forma continua hasta que a llegue a 500 (primer hilo) o a -500
+        //(segundo hilo). Además se mostrará en pantalla la variable cada vez que cambie
+        //indicando quién la ha cambiado(thread 1 o thread 2 y cada uno en un color).
+        //Ambos hilos deben parar en cuanto uno consiga su objetivo.
+        //No uses setCursorPosition.
+        //El Main, una vez que lanza los hilos, se queda en espera hasta que ambos hilos
+        //finalizan, luego informa de cual ha ganado.
+        //b) Las funciones de hilos serán expresiones lambda(si quieres y los ves claro haz
+        //ya directamente este apartado).
+        static bool flag = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 0;
+            Thread threadIncrementa = new Thread(() =>
+            {
+                while (flag)
+                {
+                    if (flag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"{num,10}");
+                        num++;
+                        if (num > 500 || num < -500)
+                        {
+                            flag = false;
+                        }
+                    }
+                }
+
+            });
+            Thread threadDecrementa = new Thread(() =>
+            {
+                while (flag)
+                {
+                    if (flag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{num,10}");
+                        num--;
+                        if (num > 500 || num < -500)
+                        {
+                            flag = false;
+                        }
+                    }
+                }
+
+            });
+            threadIncrementa.Start();
+            threadDecrementa.Start();
         }
     }
 }
