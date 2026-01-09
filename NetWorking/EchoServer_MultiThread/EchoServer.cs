277c4fe9b2adc7ed328fe61fbb87d5ada0b72e55
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-01-09 10:42:03 +0100

    changes

---
+++ NetWorking/EchoServer_MultiThread/EchoServer.cs
@@ -0,0 +1,10 @@
+﻿namespace EchoServer_MultiThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- NetWorking/Ejemplo1/Servicio_Eco.cs
+++ NetWorking/Ejemplo1/Servicio_Eco.cs
@@ -1,10 +1,81 @@
-﻿namespace Ejemplo1
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Ejemplo1
 {
-    internal class Program
+    internal class Servicio_Eco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, 31416);
+            // Creacion del Socket. Un socket es IDisposable,
+            // por tanto con using para cierre automático.
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+            ProtocolType.Tcp))
+            {
+                // Enlace de socket al puerto (y en cualquier interfaz de red)
+                // Ojo: Habría que realizar comprobación de puerto ocupado
+                // Según se vio previamente
+                s.Bind(ie);
+                // Esperando una conexión y estableciendo cola de clientes pendientes
+                s.Listen(10);
+                // Información de depuración
+
+                Console.WriteLine($"Servidor iniciado. Escuchando en {ie.Address}:{ie.Port}");
+                Console.WriteLine("Esperando conexiones... (Ctrl+C para salir)");
+                // Esperamos y aceptamos la conexion del cliente (socket bloqueante)
+                // Luego se hará en bucle para aceptar varios clientes
+                Socket sClient = s.Accept();
+
+                using (sClient) // Este using por separado pues luego irá en un hilo
+                {
+                    // Obtenemos la info del cliente. El casting es necesario ya que
+                    // RemoteEndPoint es del tipo EndPoint mas genérico
+                    IPEndPoint ieClient = (IPEndPoint)sClient.RemoteEndPoint;
+                    Console.WriteLine($"Cliente conectado:{ieClient.Address} en puerto {ieClient.Port}");
+
+                    // Aquí se cierra la conexión,
+                    // luego se atenderá al cliente en un hilo
+
+
+                    Encoding codificacion = Console.OutputEncoding; //Leemos la codificación para usar la misma en la sobrecarga de los Streams
+                    using (NetworkStream ns = new NetworkStream(sClient))
+                    using (StreamReader sr = new StreamReader(ns, codificacion))
+                    using (StreamWriter sw = new StreamWriter(ns, codificacion))
+                    {
+                        sw.AutoFlush = true; // Flush automático.
+                        string welcome = "Welcome to The Echo-Logic, Odd, Desiderable, " +
+                        "Incredible, and Javaless Echo Server (T.E.L.O.D.I.J.E Server)";
+                        sw.WriteLine(welcome);
+                        string? msg = "";
+                        while (msg != null)
+                        {
+                            try
+                            {
+                                // Leemos el mensaje del cliente
+                                msg = sr.ReadLine();
+                                // Si el cliente manda mensaje se le envía de vuelta
+                                // pero si manda null es que ha desconectado.
+                                if (msg != null)
+                                {
+                                    Console.WriteLine($"El cliente dice {msg}");
+                                    sw.WriteLine($"El servidor dice {msg}");
+                                }
+                            }
+                            // Si se cierra el cliente, salta excepción
+                            // Al siguiente readline que será null
+                            catch (IOException)
+                            {
+                                msg = null;
+                            }
+                        }
+                        Console.WriteLine("Cliente desconectado.\nConexión cerrada");
+                    }
+                }
+            }
         }
     }
 }
