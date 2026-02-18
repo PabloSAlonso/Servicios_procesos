e938d5f5e6dda8ea9ef30fa5e5253a54cad6b77d
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-02-18 13:46:45 +0100

    changes

---
+++ NetWorking/Ejercicio3_Servidor/Record.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3_Servidor
+{
+    public class Record
+    {
+        private string nombre;
+        private int segundos;
+
+        public string Nombre
+        {
+            set
+            {
+                nombre = value.Substring(0, 3);
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+
+        public int Segundos
+        {
+            set
+            {
+                segundos = value;
+            }
+            get
+            {
+                return segundos;
+            }
+        }
+
+        public Record(string nombre, int segundos)
+        {
+            Nombre = nombre;
+            Segundos = segundos;
+        }
+    }
+}
--- NetWorking/Ejercicio4_Servidor/Servidor_Records.cs
+++ NetWorking/Ejercicio4_Servidor/Servidor_Records.cs
@@ -10,14 +10,62 @@ namespace Ejercicio4_Servidor
 {
     internal class Servidor_Records
     {
-        bool serverRunning = false;
-        int Port = -1;
+        bool serverRunning = true;
+        int Port = 31416;
         Socket socketServer;
         string rutaPalabras = $"{Environment.GetEnvironmentVariable("programdata")}\\palabras.txt";
         string rutaRecords = $"{Environment.GetEnvironmentVariable("programdata")}\\records.dat";
 
+        public bool PuertoLibre(int puerto)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, puerto);
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    s.Bind(iPEndPoint);
+                    s.Listen(10);
+
+                }
+                catch (SocketException e)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public int GestionarPuertoOcupado(int puerto)
+        {
+            IPEndPoint iP = new IPEndPoint(IPAddress.Any, puerto);
+            bool puertoValido = false;
+            while (!puertoValido && puerto <= IPEndPoint.MaxPort)
+            {
+                using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+
+                    try
+                    {
+                        s.Bind(iP);
+                        s.Listen(10);
+                        puertoValido = true;
+                    }
+                    catch (SocketException e)
+                    {
+                        puerto++;
+                    }
+                    return puerto;
+                }
+            }
+            return -1;
+        }
+
         public void InitServer()
         {
+            if (!PuertoLibre(Port))
+            {
+                GestionarPuertoOcupado(Port);
+            }
             Console.WriteLine($"Puerto {Port}");
             IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, Port);
             using (socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
@@ -25,14 +73,75 @@ namespace Ejercicio4_Servidor
                 socketServer.Bind(iPEndPoint);
                 socketServer.Listen(1);
                 Console.WriteLine("Usuario conectado");
+                while (serverRunning)
+                {
+                    try
+                    {
+                        Socket client = socketServer.Accept();
+                        Thread hiloClient = new Thread(() => ClientDispatcher(client));
+                        hiloClient.Start();
+                    }
+                    catch (SocketException s) { }
+                }
+            }
+        }
+        public void ClientDispatcher(Socket sClient)
+        {
+            using (sClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint ip = new IPEndPoint(IPAddress.Any, Port);
+                using (NetworkStream network = new NetworkStream(sClient))
+                using (StreamReader sr = new StreamReader(network, Console.OutputEncoding))
+                using (StreamWriter sw = new StreamWriter(network, Console.OutputEncoding))
+                {
+                    sw.AutoFlush = true;
+                    sw.WriteLine("Bienvenido al servidor de Ahorcado");
+                    sw.WriteLine("Introduce nombre para guardar tus records");
+                    string nombre = sr.ReadLine();
+                    sw.WriteLine("Comandos disponibles: ( gw | sw 'palabra' | gr | sr 'record' | close 'clave' )");
+                    string comando = sr.ReadLine();
+                    string[] comandoEntero = new string[2];
+                    if (comando != null)
+                    {
+                        comandoEntero = comando.Split(',');
+                    }
+                    switch (comandoEntero[0])
+                    {
+                        case "gw":
 
+                            break;
 
+                        case "sw":
+
+                            break;
+                        case "gr":
+
+                            break;
+                        case "sr":
+
+                            break;
+                        case "close":
+
+                            break;
+                    }
+                }
             }
         }
 
-        public void ClientDispatcher(Socket sClient)
+        public void StopServer()
         {
-
+            serverRunning = false;
+            socketServer.Close();
         }
+
+        public string[] PalabrasArchivo(string ruta)
+        {
+            using (StreamReader sr = new StreamReader(ruta))
+            {
+                return sr.ReadToEnd().Split(",");
+            }
+        }
+
+        public List<Record>
     }
 }
